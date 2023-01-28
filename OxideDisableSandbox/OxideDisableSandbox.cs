45134dd28a701b81686d983921612bb89bd6273b
RFC1920 <desolationoutpostpve@gmail.com>
2023-01-28 12:31:56 -0600

    Initial import

---
+++ OxideDisableSandbox/OxideDisableSandbox.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using Harmony;
+using Oxide.Plugins;
+
+namespace OxideDisableSandbox
+{
+    [HarmonyPatch(typeof(CSharpExtension), "Load")]
+    public static class OxideDisableSandbox
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr, ILGenerator il)
+        {
+            //System.Reflection.MethodInfo arrayReference = typeof(Array).GetMethod("Empty").MakeGenericMethod(typeof(object));
+            List<CodeInstruction> codes = new List<CodeInstruction>(instr);
+
+            int startIndex = 0;
+            int i;
+            for (i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Newobj && codes[i + 1].opcode == OpCodes.Callvirt)
+                {
+                    startIndex = i + 2;
+                    break;
+                }
+            }
+
+            List<CodeInstruction> instructionsToInsert = new List<CodeInstruction>()
+            {
+                new CodeInstruction(OpCodes.Ldc_I4_0),
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(CSharpExtension), "set_SandboxEnabled")),
+            };
+            codes.InsertRange(startIndex, instructionsToInsert);
+            codes.RemoveRange(startIndex + 2, 17);
+            return codes;
+        }
+    }
+}
---
+++ OxideDisableSandbox/Sandbox.cs
@@ -0,0 +1,24 @@
+﻿using Oxide.Core;
+
+namespace OxideDisableSandbox
+{
+    public sealed class DisableSandbox
+    {
+        public string Name => "Sandbox";
+        public string Author => "RFC1920";
+        public VersionNumber Version => new VersionNumber(1, 0, 1);
+
+        public static bool isconsole;
+        private static bool debug = true;
+
+        public static void Init()
+        {
+            LogDebug("Disable Sandbox loaded.");
+        }
+
+        private static void LogDebug(string debugTxt)
+        {
+            if(debug) Interface.Oxide.LogDebug(debugTxt);
+        }
+    }
+}
